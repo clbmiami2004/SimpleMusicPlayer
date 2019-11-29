4b08010999ff58f7ea7505858804c51a8c980488
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-28 21:38:57 -0800

    Created 5 buttons and put them on a StackView, and the aligned them together.

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -2,13 +2,18 @@
 //  GenreButtonScreen.swift
 //  MusicPlayer
 //
-//  Created by Lambda_School_Loaner_201 on 11/28/19.
 //  Copyright © 2019 Christian Lorenzo. All rights reserved.
 //
 
 import UIKit
 
 class GenreButtonScreen: UIViewController {
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +22,5 @@ class GenreButtonScreen: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
