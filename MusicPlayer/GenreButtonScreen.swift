7e02e63c59148fa3809d7896d39ac86fd3247e35
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-28 22:16:18 -0800

    Cerated functions for the buttons being tapped. Created a list for the genre. "THIS APP WILL ONLY RUN IF YOU HAVE APPLE MUSIC". If you want to copy the code, please make sure you have to name your Genre the same way apple names its Genre list:
    Blues
    Comedy
    Children’s Music
    Classical
    Country
    Electronic
    Holiday
    Opera
    Singer/Songwriter
    Jazz
    Latino
    New Age
    Pop
    R&B/Soul
    Soundtrack
    Dance
    Hip-Hop/Rap
    World
    Alternative
    Rock
    Christian & Gospel
    Vocal
    Reggae
    Easy Listening
    J-Pop
    Enka
    Anime
    Kayokyoku
    Fitness & Workout
    K-Pop
    Karaoke
    Instrumental
    Brazilian
    Spoken Word
    Disney
    French Pop
    German Pop
    German Folk

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -13,14 +13,21 @@ class GenreButtonScreen: UIViewController {
     var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
     
     @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
     }
     
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
     }
     
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
     }
     
     func playGenre(genre: String) {
