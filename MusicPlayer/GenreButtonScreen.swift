f274339b2d6f464462e4be5765f66cfc67fa7f04
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-28 21:53:58 -0800

    Created the Genre function which includes the predicate, shuffle, play.

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -6,13 +6,43 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
     
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+    }
     
     
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
     
     
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+    func playGenre(genre: String) {
+        musicPlayer.stop()
+        
+        //Applying query.
+        let query = MPMediaQuery()
+        //Predicate is like a filter.
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        //Applying the filter of the Genre of the songs we're getting.
+        query.addFilterPredicate(predicate)
+        
+        //Getting back the genre we chose.
+        musicPlayer.setQueue(with: query)
+        
+        //Shuffle function.
+        musicPlayer.shuffleMode = .songs
+        
+        //Play the songs.
+        musicPlayer.play()
+    }
     
 
     override func viewDidLoad() {
